6e2f3ca4710c51291d0274265718d4430f40ccec
daba clovis <dabaclovis@yahoo.com>
2023-10-31 10:01:03 -0400

    html,livewire route added

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+
+@section('content')
+<div class="jumbotron">
+    <h1 class="display-4">welcome to etech consulting llc.</h1>
+    <p class="lead">Subtitle</p>
+    <hr class="my-4">
+    <p>This will be your solution to IT training</p>
+</div>
+@endsection
